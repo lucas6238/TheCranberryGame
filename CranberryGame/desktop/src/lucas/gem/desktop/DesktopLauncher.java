ab7bdd4c4be813805575aca04365da4c0b65d1d1
lucas6238 <lucaschacon022@gmail.com>
2017-11-04 16:43:02 -0400

    Now Works
    Sure

---
+++ CranberryGame/desktop/src/lucas/gem/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lucas.gem.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lucas.gem.CranGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CranGame(), config);
+	}
+}
